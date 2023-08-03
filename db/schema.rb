191da65bc40a8b608eb8d5005b08eddb975cbbe8
Lea Tinoso <lxthym@icloud.com>
2023-08-03 22:07:11 +0800

    Create team table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_03_135725) do
+ActiveRecord::Schema.define(version: 2023_08_03_140625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "team_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "user_id", null: false
