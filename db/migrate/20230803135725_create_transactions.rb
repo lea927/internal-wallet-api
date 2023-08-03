15d8084c8d84b55a1fdb2cebc07f9d233528e602
Lea Tinoso <lxthym@icloud.com>
2023-08-03 22:01:44 +0800

    Adds transaction table via migration & creates relations between user

---
+++ db/migrate/20230803135725_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.decimal :amount
+      t.integer :transaction_type
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_02_171357) do
+ActiveRecord::Schema.define(version: 2023_08_03_135725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "amount"
+    t.integer "transaction_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2023_08_02_171357) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "transactions", "users"
 end
