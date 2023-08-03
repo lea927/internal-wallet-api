3f2c450ad113df360c26838d9e1c1ac727293f77
Lea Tinoso <lxthym@icloud.com>
2023-08-03 22:36:31 +0800

    Creates relationship between stock and user/team, transaction models

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,5 @@
 class Stock < ApplicationRecord
+  has_many :transactions
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :teams
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :transactions
   belongs_to :team
+  has_and_belongs_to_many :stocks
 end
