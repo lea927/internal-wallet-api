507f559684d2f06c2cc95e47274997e55ab1f65f
Lea Tinoso <lxthym@icloud.com>
2023-08-03 22:32:38 +0800

    Creates stock table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_03_142116) do
+ActiveRecord::Schema.define(version: 2023_08_03_143154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "stock_name"
+    t.decimal "current_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "team_name"
