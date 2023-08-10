d61b1fc8a99b072a73cc8d4988b595fc71fa2436
Lea Tinoso <lxthym@icloud.com>
2023-08-11 01:39:12 +0800

    Adds functionality to save user or team id

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,12 +13,19 @@ class TransactionsController < ApplicationController
     target_account_number = params[:transaction]['target_account_number']
     source_account_number = params[:transaction]['source_account_number']
 
-    target_account = Transaction.find_target_account(target_account_number)
+    account_exists = Transaction.account_exists?(target_account_number)
+    source_account = Transaction.find_user_or_team(source_account_number)
+    @target_account = Transaction.find_user_or_team(target_account_number)
 
-    if target_account
+    if account_exists
       @transaction.target_wallet_id = target_account_number
       @transaction.source_wallet_id = source_account_number
       @transaction.transaction_type = :credit
+      if source_account.kind_of?(User)
+        @transaction.user_id = source_account.id
+      else
+        @transaction.team_id = source_account.id
+      end
       if @transaction.save
         redirect_to @transaction
       else
@@ -33,9 +40,15 @@ class TransactionsController < ApplicationController
   end
 
   private
-  
+
   def create_debit_transaction
     return unless @transaction.persisted?
+
+    if @target_account.kind_of?(User)
+      @transaction.user_id = @target_account.id
+    else
+      @transaction.team_id = @target_account.id
+    end
 
     debit_transaction = Transaction.new({
                                           amount: @transaction.amount,
@@ -43,6 +56,12 @@ class TransactionsController < ApplicationController
                                           source_wallet_id: @transaction.target_wallet_id,
                                           transaction_type: :debit
                                         })
+
+    if @target_account.kind_of?(User)
+      debit_transaction.user_id = @transaction.user_id
+    else
+      debit_transaction.team_id = @transaction.team_id
+    end
 
     unless debit_transaction.save
       Rails.logger.error("Failed to create corresponding debit transaction for Transaction ID: #{@transaction.id}")
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,7 +6,7 @@ class Transaction < ApplicationRecord
 
   enum transaction_type: { credit: 0, debit: 1 }
 
-  def self.find_target_account(account_number)
+  def self.account_exists?(account_number)
     result = false
     user = User.where(account_number: account_number).first
     if user.present?
@@ -19,5 +19,19 @@ class Transaction < ApplicationRecord
     end
 
     result
+  end
+
+  def self.find_user_or_team(account_number)
+    user = User.where(account_number: account_number).first
+    if user.present?
+      account = user
+    end
+
+    team = Team.where(account_number: account_number).first
+    if team.present?
+      account = team
+    end
+
+    account
   end
 end
