7894405e786b059d87d7af56c604b47075e6a60e
Lea Tinoso <lxthym@icloud.com>
2023-08-12 14:24:40 +0800

    Adds initial login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username], password_digest: params[:password])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash.now[:alert] = "Invalid username or password."
+      render :new
+    end
   end
 
   def destroy
